76601f8ceda2d0abc91b2d74d2224ea16efd6167
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-09-13 14:39:54 +0700

    add : setup

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+// Connect To MongoDB
+const URL = process.env.MONGODB_URL
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log('Connected to MONGODB');
+})
+
+app.get('/' , (req,res) => {
+    res.json({msg: 'Welcome to my Website'});
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT : ${PORT}`);
+})
