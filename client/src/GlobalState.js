958fe5248778c2cdbde0aa42907e9a37f9997e6f
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-09-14 21:21:25 +0700

    add : login register

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,11 +1,24 @@
 import React, { createContext, useState } from 'react'
 import ProductsAPI from './api/ProductsAPI'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 export const GlobalState = createContext()
 
 export const DataProvider = ({ children }) => {
 
     const [token, setToken] = useState(false)
+
+    const refreshToken = async () => {
+        const res = await axios.get('/user/refresh_token')
+
+        setToken(res.data.accestoken)
+    }
+
+    useEffect(() => {
+        const firstLogin = localStorage.getItem('firstLogin')
+        if (firstLogin) refreshToken()
+    }, [])
 
     const state = {
         token: [token, setToken],
