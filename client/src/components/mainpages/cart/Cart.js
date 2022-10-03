2e50137df9e543e2ebd039e01e9c781c55bf6fcb
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-10-03 16:18:49 +0700

    modify : cart component

--- client/src/components/mainpages/cart/Cart.js
+++ client/src/components/mainpages/cart/Cart.js
@@ -77,29 +77,29 @@ function Cart() {
 
 
     if (cart.length === 0)
-        return <h2 style={{ textAlign: "center", fontSize: "5rem" }}>Cart Empty</h2>
+        return <h2 style={{ fontFamily: "Inter Tight", marginTop: "40px", textAlign: "center", fontSize: "5rem" }}>Cart Empty</h2>
 
     return (
         <div>
             {
                 cart.map(product => (
-                    <div className="detail cart" key={product._id}>
-                        <img src={product.images.url} alt="" />
+                    <div className="w-full flex justify-around flex-wrap p-[50px] text-[150%] relative border-[1px] border-[solid] border-[#ccc] mt-[60px]" key={product._id}>
+                        <img src={product.images.url} alt="" className="max-w-[400px] w-full m-[20px] h-[450px] object-cover block" />
 
-                        <div className="box-detail">
-                            <h2>{product.title}</h2>
+                        <div className="max-w-[500px] w-full m-[5px_20px]">
+                            <h2 className="uppercase text-[#0078AA] tracking-[2px] font-[2rem] font-extrabold font-tight">{product.title}</h2>
 
-                            <h3>$ {product.price * product.quantity}</h3>
-                            <p>{product.description}</p>
-                            <p>{product.content}</p>
+                            <h3 className="mt-[10px] text-[#D2001A] font-tight">{product.price * product.quantity} Bath</h3>
+                            <p className="leading-[1.5] m-[10px_0] opacity-80 font-tight">{product.description}</p>
+                            <p className="leading-[1.5] m-[10px_0] opacity-80 font-tight">{product.content}</p>
 
-                            <div className="amount">
-                                <button onClick={() => decrement(product._id)}> - </button>
-                                <span>{product.quantity}</span>
-                                <button onClick={() => increment(product._id)}> + </button>
+                            <div >
+                                <button onClick={() => decrement(product._id)} className="w-[40px] h-[40px] border-[1px] border-solid border-[#ccc]"> - </button>
+                                <span className="text-[#D2001A] p-[0_20px]">{product.quantity}</span>
+                                <button onClick={() => increment(product._id)} className="w-[40px] h-[40px] border-[1px] border-solid border-[#ccc]"> + </button>
                             </div>
 
-                            <div className="delete"
+                            <div className="absolute top-0 right-[5px] text-[#D2001A] font-black cursor-pointer"
                                 onClick={() => removeProduct(product._id)}>
                                 X
                             </div>
@@ -108,8 +108,8 @@ function Cart() {
                 ))
             }
 
-            <div className="total">
-                <h3>Total: $ {total}</h3>
+            <div className="w-full h-[50px] flex items-center justify-between">
+                <h3 className="text-[#D2001A] font-bold font-tight">Total: $ {total}</h3>
                 <PaypalButton
                     total={total}
                     tranSuccess={tranSuccess} />
